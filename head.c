b79a6f4f227ede98e3a391b04a922ede204f4169
bcgood98 <bengood98@gmail.com>
2021-02-19 20:47:16 -0900

    Tried to combine the changes

--- head.c
+++ head.c
@@ -1,7 +1,14 @@
+/*Assignment 1 Problem 3
+Benjamin Good and Logan Chamberlain
+CSCE A321: Operating Systems
+February 18, 2021
+Implementation of the head linux function. Arguments may be passed in the form -n [num] [filename] or as [filename] -n [num]. Will read from standard input if no filename is supplied. 
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <errno.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -9,98 +16,39 @@
 #include <unistd.h>
 #include <math.h>
 
-int calcExp(int b, int e);
-
-int convertToInt(char *str) {
-	int i;
-	char c;
-	int digit;
-	int res = 0;
-	int incCounter = 0;
-	for (i = 0; str[i] != '\0'; i++) {
-		incCounter++;
-	}
-	i = 0;
-	while (incCounter > 0) {
-		c = str[i];
-		switch (c) {
-			case '0':
-				digit = 0;
-				break;
-			case '1':
-				digit = 1;
-				break;
-			case '2':
-				digit = 2;
-				break;
-			case '3':
-				digit = 3;
-				break;
-			case '4':
-				digit = 4;
-				break;
-			case '5':
-				digit = 5;
-				break;
-			case '6':
-				digit = 6;
-				break;
-			case '7':
-				digit = 7;
-				break;
-			case '8':
-				digit = 8;
-				break;
-			case '9':
-				digit = 9;
-				break;
-			default:
-				printf("An issue occurred while converting digits.");
-		}
-		if (incCounter == 1) {
-		    res += digit;
-		} else {
-		    res += calcExp(10, incCounter-1) * digit;
-		}
-		incCounter--;
-		i++;
-	}
-	return res;
-}
-
-int calcExp(int b, int e) {
-	int x = 1;
-	for (int i = 0; i < e; i++) {
-		x *= b;
-	}
-	return x;
-}
+long calcExp(int b, int e);
+int convertToInt(char *str);
+int strComp(char str1[], char str2[]);
 
 int main(int argc, char **argv) {
 	int fd;
 	char *buff;
 	char *fileName;
-	bool passedFile = false;
-	int n = (int) pow(2, 64) - 1;
+	int passedFile = 0;
+	long n =  calcExp(2, 30) - 1;
 	int lines;
 	int i;
 	int linesRead;
 	
 	buff = (char *) malloc(n);
 	
-	if (argc == 4 && strcmp(argv[2], "-n") == 0) {
+	// Handle case:  number -n
+	if (argc == 4 && strComp(argv[2], "-n")) {
 		lines = convertToInt(argv[3]);
 		fileName = argv[1];
-		passedFile = true;
-	} else if (argc == 4 && strcmp(argv[1], "-n") == 0) {
+		passedFile = 1;
+	} // Handle case: num -n <file>
+	else if (argc == 4 && strComp(argv[1], "-n")) {
 		lines = convertToInt(argv[2]);
 		fileName = argv[3];
-		passedFile = true;
-	} else if (argc == 2) {
+		passedFile = 1;
+	} // Handle case: <file>
+	else if (argc == 2) {
 		lines = 10;
-		passedFile = true;
+		passedFile = 1;
 		fileName = argv[1];
-	} else {
+	} // Handle case: read from standard input
+	else {
 		lines = 10;
 	}
 	
@@ -112,10 +60,22 @@ int main(int argc, char **argv) {
 			exit(1);
 		}
 		endPoint = read(fd, buff, n);
+		//If error reading file, exit
+		if(endPoint == -1){
+		  fprintf(stderr, "Error reading file: %s\n", strerror(errno));
+		  return 1;
+		}
 		buff[endPoint] = '\0';
-		close(fd);
+		if(close(fd) < 0){
+		  fprintf(stderr, "Failed to close file %s\n", strerror(errno));
+		  return 1;
+		}
 	} else {
 		endPoint = read(0, buff, n);
+		if(endPoint == -1){
+            fprintf(stderr, "Error reading file: %s\n", strerror(errno));
+            return 1;
+        }
 		buff[endPoint] = '\0';
 	}
 	
@@ -135,9 +95,102 @@ int main(int argc, char **argv) {
 			output[i] = buff[i];
 	}
 	free(buff);
-	printf("%s\n", output);
+	write(1, output, endPoint);
 	free(output);
 	
 	return 0;
 	
+}
+
+//calcExp implements math.pow functionality, reuturns a long value as exponents grow large quickly. 
+long calcExp(int b, int e) {
+  long  x = 1;
+        for (int i = 0; i < e; i++) {
+                x *= b;
+        }
+        return x;
+	}
+//strComp implements strcmp, will return 1 if the given strings are equal and 0 if not. 	  
+ int strComp(char str1[], char str2[]){
+	  int result, len1, len2, i;
+	  result = 0;
+	  len1 = 0;
+	  len2 = 0;
+	  i = 0;
+	  while(str1[i] != NULL){
+	    len1++;
+	    i++;
+	  }
+	  i = 0;
+	  while(str2[i] != NULL){
+	    len2++;
+	    i++;
+	  }
+	  if(len1 != len2){
+	    return 0;
+	  }
+	  for(i = 0; i < len1; i++){
+	    if(str1[i] != str2[i]){
+	      return 0;
+	    }
+	  }
+	  return 1;
+	}
+//convertToInt implements atoi, returns the integer equivelent of a string number, will return an error message if not all numbers 0-9.   
+int convertToInt(char *str) {
+        int i;
+        char c;
+        int digit;
+        int res = 0;
+        int incCounter = 0;
+	// printf("str is %s \n", str);
+        for (i = 0; str[i] != '\0'; i++) {
+                incCounter++;
+        }
+        i = 0;
+        while (incCounter > 0) {
+                c = str[i];
+                switch (c) {
+                        case '0':
+                                digit = 0;
+                                break;
+                        case '1':
+                                digit = 1;
+                                break;
+                        case '2':
+                                digit = 2;
+                                break;
+                        case '3':
+                                digit = 3;
+                                break;
+                        case '4':
+                                digit = 4;
+                                break;
+                        case '5':
+                                digit = 5;
+                                break;
+                        case '6':
+                                digit = 6;
+                                break;
+                        case '7':
+                                digit = 7;
+                                break;
+                        case '8':
+                                digit = 8;
+                                break;
+                        case '9':
+                                digit = 9;
+                                break;
+                        default:
+                                printf("An issue occurred while converting digits.");
+                }
+                if (incCounter == 1) {
+                    res += digit;
+                } else {
+                    res += calcExp(10, incCounter-1) * digit;
+                }
+                incCounter--;
+                i++;
+        }
+        return res;
 }
